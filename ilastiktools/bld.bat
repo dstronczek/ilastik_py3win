17a4c3cf3522c980bfc805d1f21c50df4d4647af
David-PC <stronczek@stud.uni-heidelberg.de>
2017-09-05 17:24:45 +0200

    Fixed ilastiktool recipe

--- ilastiktools/bld.bat
+++ ilastiktools/bld.bat
@@ -1,25 +1,5 @@
-REM load toolset info
-set TOOLSET_INFO_DIR=%PREFIX%\toolset-info
-call "%TOOLSET_INFO_DIR%\common-vars.bat"
-
 mkdir build
 cd build
-
-set LIB=%PREFIX%\Library\lib;%PREFIX%\libs;%LIB%
-
-REM cmake .. -G "%CMAKE_GENERATOR%" ^
-         REM -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-         REM -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-         REM -DVIGRA_INCLUDE_DIR="%LIBRARY_PREFIX%/include" ^
-         REM -DWITH_OPENMP=ON
-
-REM cmake .. -G "%CMAKE_GENERATOR%" ^
-         REM -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-         REM -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-		 REM -DCMAKE_INCLUDE_PATH="%PREFIX%\include;%SP_DIR%\numpy\core\include;%PREFIX%\libs\python35.lib" ^
-		 REM -DPYTHON_EXECUTABLE="%PYTHON%" ^
-         REM -DVIGRA_INCLUDE_DIR="%LIBRARY_PREFIX%/include" ^
-         REM -DWITH_OPENMP=ON
 
 cmake .. -G "%CMAKE_GENERATOR%" ^
          -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
